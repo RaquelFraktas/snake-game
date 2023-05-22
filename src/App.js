9e7b9809e2146b82207045c2703cad3a443fc047
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-05-22 18:19:26 -0400

    wrap app in router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import { Routes , Route } from 'react-router-dom'
 import Home from './components/home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
