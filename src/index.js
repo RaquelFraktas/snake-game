a8eb20972ecc5ab3be21ffab26eb6111cfdf140d
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-05-24 11:34:47 -0400

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
