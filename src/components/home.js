71d246ce886ad89d78306acb4b53a822dfe78577
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-05-24 16:14:46 -0400

    include registration

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import Button from 'react-bootstrap/Button';
+import Registration from './registration'
 
 
 export default function home() {
+
     return <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
@@ -11,19 +12,7 @@ export default function home() {
           Please log in 
         </p>
         <hr className="my-4" />
-        <Button
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Log in
-        </Button>
-
-        <Button
-          className="btn btn-lg custom-button ms-"
-          role="button"
-        >
-          Create a username
-        </Button>
+        <Registration />
       </div>
     </div>
   </div>
