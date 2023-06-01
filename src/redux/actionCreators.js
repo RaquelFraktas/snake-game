7fbe64ea67963c3a5bd1fec3de6e4ddd0a64f6ee
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-06-01 10:50:24 -0400

    action creators

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,46 @@
+const api = process.env.REACT_APP_API
+//the above environmental var is for development
+//config an env variable to equal production endpoint when deploying
+
+export const submitSignUp = (user) =>{
+  return dispatch => fetch (api + "/users", {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res =>res.json())
+  .then(response => {
+    if(!response.errors){
+    localStorage.token = response.token
+    dispatch({type: "SET_USER", payload: response.user})
+    } else {
+      dispatch({type: "ERROR", payload: response.errors})
+    }
+  })
+}
+
+
+
+export const submitLogin = (user) =>{
+  return dispatch => fetch (api + "/login", {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res =>res.json())
+  .then(response => {
+    if(!response.errors){
+      localStorage.token = response.token
+      dispatch({type: "SET_USER", payload: response.user})
+    } else {
+      dispatch({type: "ERROR", payload: response.errors})
+    }
+  })
+}
+
+
+export const clearErrors = () => ({type:"CLEAR_ERROR"})
