eb11987022382bce635ec254d911f2ce63519f3b
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-06-20 13:59:39 -0400

    add auto login, snake, mapstate to props

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,29 @@
 import * as React from 'react'
 import Auth from './auth'
+import Snake from './snake'
+import { connect } from 'react-redux'
+import { autoLogin } from '../redux/actionCreators';
 
 
-export default function home() {
+
+function Home({autoLogin}) {
 
     return <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
         <h1 className="display-4">Let's Play Snake!</h1>
-        <p className="lead">
-          Please log in 
-        </p>
         <hr className="my-4" />
         <Auth />
+        {autoLogin && <Snake />}
       </div>
     </div>
   </div>
     
 }
+
+const mapStateToProps = (state)=> {
+  return {user: state.user}
+
+}
+
+export default connect(mapStateToProps, { autoLogin })(Home);
